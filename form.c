87cdcbc3b26529af2158c6cadd52180c76ae8cbb
David Crosby <dave@dafyddcrosby.com>
2015-08-05 22:21:01 -0600

    Remove unnecessary checks

--- form.c
+++ form.c
@@ -748,17 +748,12 @@ form_write_from_file(FILE * f, char *boundary, char *name, char *filename,
     fprintf(f, "Content-Type: %s\r\n\r\n",
 	    type ? type : "application/octet-stream");
 
-    if (lstat(file, &st) < 0)
-	goto write_end;
-    if (S_ISDIR(st.st_mode))
-	goto write_end;
     fd = fopen(file, "r");
     if (fd != NULL) {
 	while ((c = fgetc(fd)) != EOF)
 	    fputc(c, f);
 	fclose(fd);
     }
-  write_end:
     fprintf(f, "\r\n");
 }
 
